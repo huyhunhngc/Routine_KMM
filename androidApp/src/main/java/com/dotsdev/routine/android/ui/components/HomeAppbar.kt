e633bd22bd6f370fc49aabf5dd2a847265e9a32a
huyhunhngc <huyhn@hybrid-technologies.vn>
2023-05-19 15:10:42 +0700

    appbar

--- androidApp/src/main/java/com/dotsdev/routine/android/ui/components/HomeAppbar.kt
+++ androidApp/src/main/java/com/dotsdev/routine/android/ui/components/HomeAppbar.kt
@@ -1,16 +1,20 @@
 package com.dotsdev.routine.android.ui.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import com.dotsdev.routine.model.TaskItem
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,7 +41,7 @@ private fun HomeAppBar(
             actions = {
                 IconButton(onClick = onMoreClick) {
                     Icon(
-                        painterResource(ToDometerIcons.MoreVert),
+                        Icons.Filled.MoreVert,
                         contentDescription = ""
                     )
                 }
@@ -49,7 +53,50 @@ private fun HomeAppBar(
                 )
             }
         )
-        TaskListProgress(taskListName, tasks)
-        ToDometerDivider()
+        TaskProgress(taskListName, tasks)
+    }
+}
+
+@Composable
+internal fun HomeMoreDropdownMenu(
+    expanded: Boolean,
+    onDismissRequest: () -> Unit,
+    onEditTaskListClick: () -> Unit,
+    onDeleteTaskListClick: () -> Unit
+) {
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = onDismissRequest
+    ) {
+        DropdownMenuItem(
+            onClick = onEditTaskListClick,
+            leadingIcon = {
+                Icon(
+                    Icons.Filled.Edit,
+                    contentDescription = null
+                )
+            },
+            text = {
+                Text(
+                    "Edit",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        )
+        DropdownMenuItem(
+            onClick = onDeleteTaskListClick,
+            leadingIcon = {
+                Icon(
+                    Icons.Filled.Delete,
+                    contentDescription = null
+                )
+            },
+            text = {
+                Text(
+                    "Delete",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        )
     }
 }
