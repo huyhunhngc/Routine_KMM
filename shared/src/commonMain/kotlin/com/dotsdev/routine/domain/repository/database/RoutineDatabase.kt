d9619ccd374f7db63a61c8dc09e475f9b9c0c972
huyhunhngc <huyhn@hybrid-technologies.vn>
2023-07-31 16:03:22 +0700

    try fix duplicate generate

--- shared/src/commonMain/kotlin/com/dotsdev/routine/domain/repository/database/RoutineDatabase.kt
+++ shared/src/commonMain/kotlin/com/dotsdev/routine/domain/repository/database/RoutineDatabase.kt
@@ -1,2 +1,19 @@
 package com.dotsdev.routine.domain.repository.database
 
+import com.dotsdev.routine.RoutineDatabase
+import com.dotsdev.routine.TaskChecklistItemEntity
+import com.dotsdev.routine.TaskEntity
+import com.dotsdev.routine.domain.repository.driver.DriverFactory
+import com.squareup.sqldelight.EnumColumnAdapter
+
+fun createDatabase(): RoutineDatabase =
+    RoutineDatabase(
+        DriverFactory.createSqlDriver(),
+        TaskEntityAdapter = TaskEntity.Adapter(
+            stateAdapter = EnumColumnAdapter(),
+            tagAdapter = EnumColumnAdapter()
+        ),
+        TaskChecklistItemEntityAdapter = TaskChecklistItemEntity.Adapter(
+            stateAdapter = EnumColumnAdapter()
+        )
+    )
--- shared/src/iosMain/kotlin/com/dotsdev/routine/domain/repository/driver/DriverFactory.kt
+++ shared/src/iosMain/kotlin/com/dotsdev/routine/domain/repository/driver/DriverFactory.kt
@@ -2,9 +2,10 @@ package com.dotsdev.routine.domain.repository.driver
 
 import com.dotsdev.routine.RoutineDatabase
 import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 
-//actual object DriverFactory {
-//    actual fun provideSqlDriver(): SqlDriver {
-//        return NativeSqliteDriver(RoutineDatabase.Schema, "app.db")
-//    }
-//}
+actual object DriverFactory {
+    actual fun createSqlDriver(): SqlDriver {
+        return NativeSqliteDriver(RoutineDatabase.Schema, "app.db")
+    }
+}
